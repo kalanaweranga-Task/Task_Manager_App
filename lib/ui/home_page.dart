92b7163d15fbad6761b569a2b2d2b8227c1dbfd5
Chamya Shamindi <91967400+ChamyaShamindi@users.noreply.github.com>
2023-10-24 10:08:29 +0530

    create task

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -7,6 +7,7 @@ import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:task_management_app/controllers/task_controller.dart';
+import 'package:task_management_app/models/task.dart';
 // import 'package:google_fonts/google_fonts.dart';
 import 'package:task_management_app/services/notification_services.dart';
 import 'package:task_management_app/services/theme_services.dart';
@@ -15,6 +16,7 @@ import 'package:intl/intl.dart';
 import 'package:task_management_app/ui/add_task_bar.dart';
 import 'package:task_management_app/ui/theme.dart';
 import 'package:task_management_app/ui/widgets/button.dart';
+import 'package:task_management_app/ui/widgets/input_field.dart';
 import 'package:task_management_app/ui/widgets/task_tile.dart'; // Import the intl package
 
 class HomePage extends StatefulWidget {
@@ -25,7 +27,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  DateTime selectedDate = DateTime.now();
+  DateTime _selectedDate = DateTime.now();
   final _taskController = Get.put(TaskController());
   late NotifyHelper notifyHelper;
 
@@ -60,28 +62,151 @@ class _HomePageState extends State<HomePage> {
         return ListView.builder(
             itemCount: _taskController.taskList.length,
             itemBuilder: (_, index) {
-              print(_taskController.taskList.length);
-              return AnimationConfiguration.staggeredList(
-                position: index,
-                duration: const Duration(milliseconds: 375),
-                child: SlideAnimation(
-                  verticalOffset: 50.0,
-                  child: FadeInAnimation(
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            print("Tapped");
-                          },
-                          child: TaskTile(_taskController.taskList[index]),
-                        )
-                      ],
+              
+              Task task = _taskController.taskList[index];
+              print(task.toJson());
+              if(task.repeat=='Daily') {
+                return AnimationConfiguration.staggeredList(
+                  position: index,
+                  duration: const Duration(milliseconds: 375),
+                  child: SlideAnimation(
+                    verticalOffset: 50.0,
+                    child: FadeInAnimation(
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              _showBottomSheet(context, task);
+                            },
+                            child: TaskTile(task),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
+                );
+              };
+
+              if(task.date==DateFormat.yMd().format(_selectedDate)){
+                return AnimationConfiguration.staggeredList(
+                  position: index,
+                  duration: const Duration(milliseconds: 375),
+                  child: SlideAnimation(
+                    verticalOffset: 50.0,
+                    child: FadeInAnimation(
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              _showBottomSheet(context, task);
+                            },
+                            child: TaskTile(task),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }else{
+                return Container();
+              }
+              
             });
       }),
+    );
+  }
+
+  _showBottomSheet(BuildContext context, Task task){
+    Get.bottomSheet(
+      Container(
+        padding: const EdgeInsets.only(top: 4),
+        height: task.isCompleted==1?
+        MediaQuery.of(context).size.height*0.24:
+        MediaQuery.of(context).size.height*0.32,
+        color: Get.isDarkMode?darkGreyClr:Colors.white,
+      child: Column(
+        children: [
+          Container(
+            height: 6,
+            width: 120,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Get.isDarkMode?Colors.grey[600]:Colors.grey[300]
+            )
+          ),
+          Spacer(),
+          task.isCompleted==1
+          ?Container()
+            : _bottomSheetButton(
+              label: "Task Completed",
+              onTap: (){
+                _taskController.markTaskCompleted(task.id!);
+                Get.back();
+              },
+              clr: primaryClr,
+              context:context,
+            ),
+            
+            _bottomSheetButton(
+              label: "Delete Task",
+              onTap: (){
+                _taskController.deleteTask(task);
+                Get.back();
+              },
+              clr: Colors.red[300]!,
+              context:context,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            _bottomSheetButton(
+              label: "Close",
+              onTap: (){
+                Get.back();
+              },
+              clr: Colors.red[300]!,
+              isClose: true,
+              context:context,
+            ),
+            SizedBox(
+              height: 10,
+            )
+        ],
+        )
+      ),
+    );
+  }
+
+  _bottomSheetButton({
+    required String label,
+    required Function()? onTap,
+    required Color clr,
+    bool isClose=false,
+    required BuildContext context,
+  }){
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        height: 55,
+        width: MediaQuery.of(context).size.width*0.9,
+        
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 2,
+            color: isClose==true?Get.isDarkMode?Colors.grey[600]!:Colors.grey[300]!:clr
+          ),
+          borderRadius: BorderRadius.circular(20),
+          color: isClose==true?Colors.transparent:clr,
+        ),
+
+        child:Center(
+          child:Text(
+            label,
+            style: isClose?titleStyle:titleStyle.copyWith(color:Colors.white),
+          ),
+        )
+      )
     );
   }
 
@@ -110,7 +235,11 @@ class _HomePageState extends State<HomePage> {
                   fontSize: 14,
                   fontWeight: FontWeight.w600,
                   color: Colors.grey)),
-          onDateChange: (date) {},
+          onDateChange: (date) {
+            setState(() {
+              _selectedDate=date;
+            });
+          },
         ));
   }
 
